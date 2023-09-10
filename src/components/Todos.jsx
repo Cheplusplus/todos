bc8740c796e73193914bb13b0ab184e8fb132c7c
ChePlusPlus <chephoto@rocketmail.com>
2023-09-11 00:28:30 +0200

    Started adding the Todo component

--- src/components/Todos.jsx
+++ src/components/Todos.jsx
@@ -1,6 +1,34 @@
+//@ts-check
 import React from "react";
+import { useLocalStorage } from "../hooks/useLocalStorage";
 
+/**
+ * @typedef {Object} Todo
+ * @property {string} id
+ * @property {string} message
+ * @property {boolean} isComplete
+ */
+
+/**
+ *
+ * @param {Todo} todo
+ * @param {Todo[]} todos
+ * @param {function} setTodos
+ * @returns
+ */
+const addTodo = (todo, todos, setTodos) => {
+  setTodos([...todos, todo]);
+  return [...todos, todo];
+};
+
+/**
+ *
+ * @param {Object} p
+ * @param {boolean} p.nightMode
+ * @returns
+ */
 const Todos = ({ nightMode }) => {
+  const [todos, setTodos] = useLocalStorage("todos", []);
   return <div className={"todos" + (nightMode ? " night-todos" : "")}></div>;
 };
 
